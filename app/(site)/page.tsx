f4db1c428d52fb79e3e97aa958fa95c57f489bbf
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-08 12:54:32 -0600

    import cleanup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import ListItem, { LikedListItem } from "@/components/ListItem";
+import { LikedListItem } from "@/components/ListItem";
 
 export default function Home() {
   return (
