7ff732206dc47bec97161dfc8f7971bf645f3115
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-11 11:06:39 -0600

    added loaded and error components

---
+++ app/search/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+  return (
+    <Box className="h-full flex items-center justify-center transition">
+      <div className="text-neutral-400">Something went wrong</div>
+    </Box>
+  );
+};
+
+export default Error;
